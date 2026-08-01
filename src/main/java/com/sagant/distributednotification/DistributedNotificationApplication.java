09f0b49ceaaaf713bcee048664073ad94899d911
Asiel <asielp@verifone.com>
2026-07-31 22:05:25 -0300

    Initial commit

---
+++ src/main/java/com/sagant/distributednotification/DistributedNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.sagant.distributednotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagant/distributednotification/DistributedNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagant.distributednotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
